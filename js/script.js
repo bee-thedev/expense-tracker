c238d3b46bf4288f328268a01c5502ffc43898f3
bee-thedev <beenyshsaeed@gmail.com>
2022-08-19 23:32:33 +0500

    jquery and bootstrap bundles added with the basic functionality such as the min requirement for the expense to be positive

--- js/script.js
+++ js/script.js
@@ -17,11 +17,48 @@ class UI{
         this.itemList = [];
         this.itemID = 0;
     }
+    submitBudgetForm(){
+        const value= this.budgetInput.value;
+        if(value === "" || value < 0){
+            this.budgetFeedback.classList.add("showItem");
+            this.budgetFeedback.innerHTML = `<p> Enter a positive value</p>`;
+            //Assigning a constant t 'this' because it is coming from class UI, cannot be  used further down the function hell
+            const self = this;
+            console.log(self);
+            setTimeout(function(){
+                self.budgetFeedback.classlist.remove('showItem');
+        }, 4000);
+    }
+    }
 }
+
     function eventListeners(){
         const budgetForm = document.querySelector(".budget-form");
         const expenseForm = document.querySelector("#expense-form");
         const expenseList = document.querySelector("#expense-list");
+
+        // New UI instance 
+
+        const ui = new UI();
+
+        // Add Budget Listener for bdget submit
+
+        budgetForm,addEventListener('submit',function(event){
+            event.preventDefault();
+            ui.submitBudgetForm();
+        });
+
+        // Add Expense Listener for exense submit
+
+        expenseForm.addEventlistener("submit", function(event){
+            event.preventDefault();
+        });
+
+        //initiate expense list
+
+        expenseList.addEventlistener("click", function(){
+
+        });
     }
 
     document.addEventListener("DOMContentLoaded", function(){
